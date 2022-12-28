9bd1907073197450c7b5785445ad8c7e2be5cc84
Aditya Narayan <nlaxmi886@gmail.com>
2022-12-28 14:31:08 +0530

    Created Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { About } from "./MyComponents/About";
 
 function App() {
   return (
